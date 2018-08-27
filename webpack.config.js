4c47602ab468d15fedde9b57eb29c029aa4d6086
dujinjin <dujinjin@duiba.com.cn>
2018-08-27 09:47:24 +0800

    entries

--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,26 @@ const fs = require('fs');
 
 var entries = {};
 function getEntries(filePath) {
-    fs.readdirSync(filePath, function(err, files){
-        if (err) {
-            throw new Error(err);
+    var filePaths = fs.readdirSync(filePath);
+    filePaths.forEach(function(item){
+        var filedir = path.join(filePath, item),
+            stats = fs.statSync(filedir),
+            isFile = stats.isFile(),
+            isDir = stats.isDirectory();
+        if (isFile && path.basename(filedir) === 'index.js') {
+            entries[path.dirname(filedir)] = filedir;
         }
-        //遍历读取到的文件列表
-        files.forEach(function(filename){
-            var filedir = path.join(filePath, filename);
-            fs.statSync(filedir, function(eror, stats){
-                if (eror) {
-                    throw new Error(err)
-                    console.warn('获取文件stats失败');
-                } else {
-                    var isFile = stats.isFile();
-                    var isDir = stats.isDirectory();
-                    if(isFile && path.basename(filedir) === 'index.js'){
-                        entries[path.dirname(filedir)] = filedir;
-                        console.log(path.dirname(filedir), filedir)
-                    }
-                    if(isDir){
-                        getEntries(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
-                    }
-                }
-            })
-        })
-    })
+        if(isDir){
+            getEntries(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
+        }
+    });
 }
-
-console.log(11111111)
-getEntries('./src');
-//console.log(entries)
+getEntries('src')
 module.exports = {
-    entry: path.resolve(__dirname, './main.js'),
+    entry: entries,
     output: {
         path: path.resolve(__dirname, './dist'), // 输出的路径
-        filename: 'app/[name]_[hash:8].js'  // 打包后文件
+        filename: '[name]_[hash:8].js'  // 打包后文件
     },
     devServer: {
         inline: true,
@@ -53,9 +38,9 @@ module.exports = {
         }]
     },
      plugins: [
-　　 　　new HtmlWebpackPlugin({
-　　　　 　　template: path.resolve(__dirname, './index.html'),
-　　　　　　 inject: true
-　　　　 })
+// 　　 　　new HtmlWebpackPlugin({
+// 　　　　 　　template: path.resolve(__dirname, './index.html'),
+// 　　　　　　 inject: true
+// 　　　　 })
 　　 ]
 };
